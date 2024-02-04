875f55b4268dad426b0d5b7ab22818ed10eb8fcc
oriyaf1 <oriyaf1@gmail.com>
2024-02-05 00:10:26 +0200

    0.44

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,7 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = ' 0.43 קש"א';
+  title = ' 0.44 קש"א';
   isMenuOpen:boolean = false;
 
 }
